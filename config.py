4f7367ca5dad72f7f075d7959fc7c306d8d4222c
qimiaoxue <15011272359@163.com>
2016-11-06 15:04:47 +0800

    update email address

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'guess what'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_MAIL_SUBJECT_PREFIX = "[XueYan's Blog]"
-    FLASKY_MAIL_SENDER = "XueYan's Blog Admin <m13488699851@163.com>"
+    FLASKY_MAIL_SENDER = "XueYan's Blog Admin <15011272359@163.com>"
     FLASKY_ADMIN = os.environ.get('MAIL_USERNAME')
     FLASKY_POSTS_PER_PAGE = 25
     # FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
